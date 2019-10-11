f6c0598625397f6ecc687d5568e364392d43e28b
Liam <liam.fabiano@epitech.eu>
2019-10-12 00:16:43 +0200

    Created class for blog page and post page layout

--- src/components/Post-Layout/index.tsx
+++ src/components/Post-Layout/index.tsx
@@ -2,22 +2,24 @@ import React from "react"
 import SEO from "../SEO"
 import Layout from "../Layout"
 
-function BlogPostLayout({children, pageContext}) {
-  const {title, author, date} = pageContext.frontmatter
-  return (
-    <Layout>
-      <SEO title={title} />
-      <article>
-        <header>
-          <h1>{title}</h1>
-          <span>Author: {author}</span>
-          <br />
-          <time>Date: {date}</time>
-        </header>
-        {children}
-      </article>
-    </Layout>
-  )
+class BlogPostLayout extends React.Component {
+  public render() {
+    const {title, author, date} = this.props.pageContext.frontmatter
+    return (
+      <Layout>
+        <SEO title={title} />
+        <article>
+          <header>
+            <h1>{title}</h1>
+            <span>Author: {author}</span>
+            <br />
+            <time>Date: {date}</time>
+          </header>
+          {this.props.children}
+        </article>
+      </Layout>
+    )
+  }
 }
 
 export default BlogPostLayout
